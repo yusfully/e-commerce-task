9c4168e3650328b85f2bfbb4813beb68bfa378bc
Yusfully <yusuf06ozturk@gmail.com>
2021-02-10 17:16:47 +0300

    initial

--- server/index.js
+++ server/index.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const path = require("path");
+const cluster = require("cluster");
+const cors = require("cors");
+const numCPUs = require("os").cpus().length;
 require("./db.json");
-const PORT = 5000;
-const app = express();
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+const isDev = process.env.NODE_ENV !== "production";
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, function () {
-  console.error(
-    `Node ${"cluster worker " + process.pid}: listening on port ${PORT}`
-  );
-});
+if (!isDev && cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.error(
+      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
+    );
+  });
+} else {
+  const app = express();
+  app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+  app.listen(PORT, function () {
+    console.error(
+      `Node ${"cluster worker " + process.pid}: listening on port ${PORT}`
+    );
+  });
+}
