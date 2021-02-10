7255e6e6c9ae5e301736d4693d0cd8a17a1da1ae
Yusfully <yusuf06ozturk@gmail.com>
2021-02-10 17:29:16 +0300

    initial

--- server/index.js
+++ server/index.js
@@ -1,34 +1,10 @@
-const express = require("express");
-const path = require("path");
-const cluster = require("cluster");
-const cors = require("cors");
-const numCPUs = require("os").cpus().length;
-require("./db.json");
-const isDev = process.env.NODE_ENV !== "production";
-const PORT = process.env.PORT || 5000;
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3001;
 
-if (!isDev && cluster.isMaster) {
-  console.error(`Node cluster master ${process.pid} is running`);
+server.use(middlewares);
+server.use(router);
 
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on("exit", (worker, code, signal) => {
-    console.error(
-      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
-    );
-  });
-} else {
-  const app = express();
-  app.use(express.static(path.resolve(__dirname, "../client/build")));
-  app.get("*", function (request, response) {
-    response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-  });
-  app.listen(PORT, function () {
-    console.error(
-      `Node ${"cluster worker " + process.pid}: listening on port ${PORT}`
-    );
-  });
-}
+server.listen(port);
