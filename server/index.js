8481329e043b35a6a466647fcf6a7a0efd02dbcb
Yusfully <yusuf06ozturk@gmail.com>
2021-02-10 16:49:16 +0300

    initial

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const path = require('path');
-require('./db.json');
+const express = require("express");
+const path = require("path");
+require("./db.json");
 
 const app = express();
-app.use(express.static(path.resolve(__dirname, '../client/build')));
-app.get('*', function (request, response) {
-  response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
 app.listen(PORT, function () {
-  console.error(`Node ${isDev ? 'dev server' : 'cluster worker ' + process.pid}: listening on port ${PORT}`);
+  console.error(
+    `Node ${
+      isDev ? "dev server" : "cluster worker " + process.pid
+    }: listening on port ${PORT}`
+  );
 });
