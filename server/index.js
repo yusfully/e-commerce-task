693faf0bd698066f609a9cdfaade0224b81a7eff
Yusfully <yusuf06ozturk@gmail.com>
2021-02-10 16:57:00 +0300

    initial

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 require("./db.json");
-
+const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
